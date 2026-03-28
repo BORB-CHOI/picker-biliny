7826c3531ba825ac9a7ae235387cfb33e21fce0b
BORB <qzsec299@naver.com>
2026-03-29 01:33:59 +0900

    remove: 갈아엎고 다시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,10 @@
-import { Header } from '@/components/sections/Header';
-import { IntroAnimation } from '@/components/animations/IntroAnimation';
-import { HeroSection } from '@/components/sections/HeroSection';
-import { StorySection } from '@/components/sections/StorySection';
-import { AlternativesSection } from '@/components/sections/AlternativesSection';
-import { IsolationSection } from '@/components/sections/IsolationSection';
-import { SolutionSection } from '@/components/sections/SolutionSection';
-import { SocialCostSection } from '@/components/sections/SocialCostSection';
-import { BudgetSection } from '@/components/sections/BudgetSection';
-import { RevenueSection } from '@/components/sections/RevenueSection';
-import { BilinyProductSection } from '@/components/sections/BilinyProductSection';
-import { ChargingSection } from '@/components/sections/ChargingSection';
-import { ContactSection } from '@/components/sections/ContactSection';
-import { Footer } from '@/components/sections/Footer';
+import { IntroAnimation } from "@/components/animations/IntroAnimation";
 
 export default function Home() {
   return (
     <>
       <IntroAnimation />
-      <Header />
-      <main>
-        <HeroSection />
-        <StorySection />
-        <AlternativesSection />
-        <IsolationSection />
-        <SolutionSection />
-        <SocialCostSection />
-        <BudgetSection />
-        <RevenueSection />
-        <BilinyProductSection />
-        <ChargingSection />
-        <ContactSection />
-      </main>
-      <Footer />
+      <main></main>
     </>
   );
 }
